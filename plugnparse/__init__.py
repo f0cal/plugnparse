b80d7ce7e7ef6c36ac847c1cf31a10c17752d63b
br <brian.rossa@gmail.com>
2016-07-26 10:51:31 -0400

    Project boilerplate from `cookiecutter https://github.com/audreyr/cookiecutter-pypackage`

---
+++ plugnparse/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Brian Rossa'
+__email__ = 'brian.rossa@gmail.com'
+__version__ = '0.0.1'
---
+++ tests/test_plugnparse.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_plugnparse
+----------------------------------
+
+Tests for `plugnparse` module.
+"""
+
+
+import sys
+import unittest
+
+from plugnparse import plugnparse
+
+
+
+class TestPlugnparse(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_000_something(self):
+        pass
+
+
+
+if __name__ == '__main__':
+    sys.exit(unittest.main())
