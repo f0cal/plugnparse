45ce393889be3987ae0db948050af2e4e676afd6
Brian Rossa <br@f0cl.com>
2016-08-21 23:49:56 -0400

    Basic code cleanup. No changes to logic or interfaces.

--- plugnparse/parserfactory.py
+++ plugnparse/parserfactory.py
@@ -1,4 +1,5 @@
 import argparse
+import venusian
 
 class ParserTree(object):
     _dest_prefix = 'cmd'
@@ -16,23 +17,22 @@ class ParserTree(object):
         return tuple(item)
 
     def __getitem__(self, item):
+        _parsers = self.parsers
+        _subparsers = self.subparsers
         item = self._make_tuple(item)
         if len(item) == 0:
             return self._base_parser
-        if item in self.parsers:
-            return self.parsers[item]
+        if item in _parsers:
+            return _parsers[item]
         parent_item = item[:-1]
         parent_parser = self[parent_item]
-        if parent_item not in self.subparsers:
-            _dest = "{}{}".format(self._dest_prefix, len(parent_item))
-            self.subparsers[parent_item] = parent_parser.add_subparsers(dest=_dest)
-            self.subparsers[parent_item].required = True
-        if item not in self.parsers:
-            self.parsers[item] = self.subparsers[parent_item].add_parser(item[-1])
-        return self.parsers[item]
-
-    def parse_args(self, *args, **dargs):
-        return self._base_parser.parse_args(*args, **dargs)
+        if parent_item not in _subparsers:
+            _d = "{}{}".format(self._dest_prefix, len(parent_item))
+            _subparsers[parent_item] = parent_parser.add_subparsers(dest=_d)
+            _subparsers[parent_item].required = True
+        if item not in _parsers:
+            _parsers[item] = _subparsers[parent_item].add_parser(item[-1])
+        return _parsers[item]
 
 class ParserFactory(object):
     def __init__(self, base=None):
@@ -43,6 +43,7 @@ class ParserFactory(object):
         self._captured_parse_known_args = base.parse_known_args
         base.parse_args = self._parse_args
         base.parse_known_args = self._parse_known_args
+        self._base = base
 
     def read_annotated_class(self, cls):
         for attr in dir(cls):
@@ -51,10 +52,12 @@ class ParserFactory(object):
             cmds = attr.replace('cli_', '').split('_')
             self._tree[cmds].set_defaults(func=attr)
 
-    def read_package(self, package, target=None):
-        scanner = venusian.Scanner(entrypoints=[], setopts=[])
+    def read_package(self, package, require=None):
+        # TODO (br) Make 'entrypoints' global
+        scanner = venusian.Scanner(entrypoints=[])
         scanner.scan(package)
         for cmds, arg_factory, fn in scanner.entrypoints:
+            # TODO (br) Make 'func' global
             self._tree[cmds].set_defaults(func=fn)
             arg_factory(self._tree[cmds])
 
@@ -62,10 +65,7 @@ class ParserFactory(object):
         return self._captured_parse_args(*args, **dargs)
 
     def _parse_known_args(self, *args, **dargs):
-        ns, _ = self._captured_parse_known_args(*args, **dargs)
-        def _run():
-            if isinstance(ns.func, str):
-                return getattr(ns.api, ns.func)(ns, self._tree._base_parser)
-            return ns.func(ns.api, ns, self._tree._base_parser)
-        setattr(ns, 'run', _run)
-        return ns, _
+        return self._captured_parse_known_args(*args, **dargs)
+
+    def make_parser(self):
+        return self._base
