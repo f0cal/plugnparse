2f094beddb224ebcd8d97b0f5f93367afb0bace5
Brian Rossa <br@f0cal.com>
2019-02-04 16:15:01 -0500

    Added convenience function that I seemed to be implementing over and over.

--- plugnparse/__init__.py
+++ plugnparse/__init__.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
 
 __author__ = 'Brian Rossa'
-__email__ = 'br@f0cl.com'
+__email__ = 'br@f0cal.com'
 __version__ = '0.0.1'
 
 from .decorators import *
 from .parserfactory import ParserFactory
+
+def scan_and_run(package_name, base_parser=None):
+    parser = base_parser or __import__('argparse').ArgumentParser()
+    factory = ParserFactory(base=parser)
+    factory.read_package(__import__(package_name))
+    parser = factory.make_parser()
+    ns = parser.parse_args()
+    return ns.func(ns, parser)
